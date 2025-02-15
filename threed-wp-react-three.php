6449e454c116481a55fa62a9ab1b0d33e08c25a3
Marty McGee <support@companyjuice.com>
2025-02-15 15:03:41 -0800

    🥕 v0.0.1 🌱 ThreeD: WordPress React-Three

--- threed-wp-react-three.php
+++ threed-wp-react-three.php
@@ -23,3 +23,24 @@ function r3f_enqueue_scripts() {
     );
 }
 add_action('wp_enqueue_scripts', 'r3f_enqueue_scripts');
+
+function r3f_shortcode() {
+    return '<div id="r3f-root"></div>';
+}
+add_shortcode('threed_wp_react_three', 'r3f_shortcode');
+
+function r3f_inject_react_app() {
+    ?>
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            const rootElement = document.getElementById('r3f-root');
+            if (rootElement) {
+                const script = document.createElement('script');
+                script.src = '<?php echo plugin_dir_url(__FILE__) . 'react-app/build/static/js/main.d76af341.js'; ?>';
+                document.body.appendChild(script);
+            }
+        });
+    </script>
+    <?php
+}
+add_action('wp_footer', 'r3f_inject_react_app');
