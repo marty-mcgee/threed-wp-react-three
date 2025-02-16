c4abe021efd0982872b51b67ce64bd87a7cb6a44
Marty McGee <support@companyjuice.com>
2025-02-16 08:50:03 -0800

    🥕 v0.0.16 🌱 ThreeD: WordPress React-Three

--- threed-wp-react-three/src/App.tsx
+++ threed-wp-react-three/src/App.tsx
@@ -1,24 +1,42 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
-import { Box } from '@react-three/drei';
-// import logo from './logo.svg';
-import './App.css';
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { Box as BoxDREI } from '@react-three/drei'
+
+import BoxThreeD from './components/BoxThreeD'
+
+// import logo from './logo.svg'
+import './App.css'
 
 const ThreeScene: React.FC = () => {
+
+  // This reference will give us direct access to the THREE.Mesh object
+  const mesh = useRef(null)
+
+  // Subscribe this component to the render-loop, rotate the mesh every frame
+  useFrame((state, delta) =>
+    mesh.current
+      // @ts-expect-error
+      ? (mesh.current.rotation.y = mesh.current.rotation.x += 0.01)
+      : null
+  )
+
   return (
     <Canvas>
       <ambientLight />
       <pointLight position={[10, 10, 10]} />
-      <Box position={[0, 0, 0]}>
+      {/* <BoxDREI 
+        position={[0, 0, 0]}
+      >
         <meshStandardMaterial color="orange" />
-      </Box>
+      </BoxDREI> */}
+      <BoxThreeD route='/' />
     </Canvas>
-  );
-};
+  )
+}
 
 function App() {
-  console.debug('DOM fully loaded, initializing React app...');
-  console.debug('Root element:', document.getElementById('threed-root'));
+  console.debug('DOM fully loaded, initializing React app...')
+  console.debug('Root element:', document.getElementById('threed-root'))
   return (
     <div className="App">
       <header className="App-header">
@@ -39,7 +57,7 @@ function App() {
       <ThreeScene />
       
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
