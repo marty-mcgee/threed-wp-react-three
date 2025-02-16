f581e821c525049a073d785b67dc422b45e486c3
Marty McGee <support@companyjuice.com>
2025-02-16 07:01:45 -0800

    🥕 v0.0.13 🌱 ThreeD: WordPress React-Three

--- threed-wp-react-three.php
+++ threed-wp-react-three.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ThreeD WP React Three
 Description: Embed a Three.js React-Three-Fiber canvas in a WordPress page using a shortcode
-Version: 0.0.12
+Version: 0.0.13
 Author: Marty McGee
 */
 
@@ -19,18 +19,18 @@ function threed_wp_react_three_enqueue_scripts() {
         // Check if the post content contains the [threed_wp_react_three] shortcode
         if (isset($post) && has_shortcode($post->post_content, 'threed_wp_react_three')) {
             // Enqueue React app's JavaScript and CSS
-            wp_enqueue_script(
-                'threed-wp-react-three-script',
-                plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.a9dfc8c4.js',
-                array(),
-                '1.0',
-                [
-                    // strategy: 
-                    'defer',
-                    // in_footer:
-                    true 
-                ]
-            );
+            // wp_enqueue_script(
+            //     'threed-wp-react-three-script',
+            //     plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.a9dfc8c4.js',
+            //     array(),
+            //     '1.0',
+            //     [
+            //         // strategy: 
+            //         'defer',
+            //         // in_footer:
+            //         true 
+            //     ]
+            // );
             wp_enqueue_style(
                 'threed-wp-react-three-style',
                 plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/css/main.f855e6bc.css',
@@ -50,7 +50,7 @@ function threed_wp_react_three_enqueue_scripts() {
 }
 add_action('wp_enqueue_scripts', 'threed_wp_react_three_enqueue_scripts');
 
-/*
+/**/
 function threed_wp_react_three_inject() {
     ?>
     <script type="text/javascript">
@@ -66,4 +66,4 @@ function threed_wp_react_three_inject() {
     <?php
 }
 add_action('wp_footer', 'threed_wp_react_three_inject');
-*/
+/**/
