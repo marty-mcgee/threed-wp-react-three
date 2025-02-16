f456a752ca1075233fded256c9c2caa4f180280c
Marty McGee <support@companyjuice.com>
2025-02-16 07:09:56 -0800

    🥕 v0.0.14 🌱 ThreeD: WordPress React-Three

--- threed-wp-react-three.php
+++ threed-wp-react-three.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: ThreeD WP React Three
 Description: Embed a Three.js React-Three-Fiber canvas in a WordPress page using a shortcode
-Version: 0.0.13
+Version: 0.0.14
 Author: Marty McGee
 */
 
@@ -21,7 +21,7 @@ function threed_wp_react_three_enqueue_scripts() {
             // Enqueue React app's JavaScript and CSS
             // wp_enqueue_script(
             //     'threed-wp-react-three-script',
-            //     plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.a9dfc8c4.js',
+            //     plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.87fe3062.js',
             //     array(),
             //     '1.0',
             //     [
@@ -58,7 +58,7 @@ function threed_wp_react_three_inject() {
             const rootElement = document.getElementById('threed-root');
             if (rootElement) {
                 const script = document.createElement('script');
-                script.src = '<?php echo plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.a9dfc8c4.js'; ?>';
+                script.src = '<?php echo plugin_dir_url(__FILE__) . 'threed-wp-react-three/build/static/js/main.87fe3062.js'; ?>';
                 document.body.appendChild(script);
             }
         });
--- threed-wp-react-three/src/App.tsx
+++ threed-wp-react-three/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Box } from '@react-three/drei';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 const ThreeScene: React.FC = () => {
   return (
     <Canvas>
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      <pointLight position={[100, 100, 100]} />
       <Box position={[0, 0, 0]}>
         <meshStandardMaterial color="orange" />
       </Box>
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
